0ee4c3ecc72f44f5fd3de507503945b193d65dcb
Kath <kathleen.cram@dmail.dixie.edu>
2018-07-11 12:19:18 -0600

    events view

--- client/data.js
+++ client/data.js
@@ -4,6 +4,48 @@ const data = () => ({
     modal: false,
     menu2: false,
     dialog: false,
+    show: false,
+    leads: [
+        {
+            name: 'event 1',
+            type: 'Construction',
+            date:  'July 30th, 2018',
+            address:  '123 St. Utah',
+            time: '12:30',
+            notes: 'these are extra notes',
+            show: false,
+        },
+        {
+            name: 'event 2',
+            type: 'Wedding',
+            date:  'July 30th, 2018',
+            address:  '123 St. Utah',
+            time: '4:10',
+            notes:'notes',
+            show: false,
+        },
+        {
+            name: 'event 3',
+            type: 'Construction',
+            date:  'July 30th, 2018',
+            address:  '123 St. Utah',
+            time: '12:30',
+            notes: 'these are extra notes',
+            show: false,
+        },
+        {
+            name: 'event 4',
+            type: 'Wedding',
+            date:  'July 30th, 2018',
+            address:  '123 St. Utah',
+            time: '4:10',
+            notes:'some notes',
+            show: false,
+        }
+
+    ],
+
+    
 })
 
 
