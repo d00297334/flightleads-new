b122855e1a2fc5548c0366ec1cbc0452be91dcf7
Kath <kathleen.cram@dmail.dixie.edu>
2018-07-16 12:06:18 -0600

    update and save functionality, computed values

--- client/data.js
+++ client/data.js
@@ -1,11 +1,12 @@
+
 const data = () => ({
-    date: '',
+    date: null,
     menu: false,
     modal: false,
     menu2: false,
     dialog: false,
     show: false,
-    time: '',
+    time: null,
     timeMenu: false,
     name: '',
     email: '',
@@ -13,52 +14,14 @@ const data = () => ({
     address: '',
     type: '',
     notes: '',
-    
-    leads: [
-        {
-            name: 'event 1',
-            type: 'Construction',
-            date:  'July 30th, 2018',
-            address:  '123 St. Utah',
-            time: '12:30',
-            notes: 'these are extra notes',
-            show: false,
-        },
-        {
-            name: 'event 2',
-            type: 'Wedding',
-            date:  'July 30th, 2018',
-            address:  '123 St. Utah',
-            time: '4:10',
-            notes:'notes',
-            show: false,
-        },
-        {
-            name: 'event 3',
-            type: 'Construction',
-            date:  'July 30th, 2018',
-            address:  '123 St. Utah',
-            time: '12:30',
-            notes: 'these are extra notes',
-            show: false,
-        },
-        {
-            name: 'event 4',
-            type: 'Wedding',
-            date:  'July 30th, 2018',
-            address:  '123 St. Utah',
-            time: '4:10',
-            notes:'some notes',
-            show: false,
-        }
-
-    ],
-
-    
-   
+    id: null,
+    editingId: null,
+    visible: null,
     dateFilters: ['Today', 'This Week', 'This Month'],
     typeFilters: ['Videography', 'Photography', 'Land/Site Survey', 'Real Estate', 'Outdoor/Extreme Sports - Event', 'Outdoor/Extreme Sports - Personal', 'Other'],
     statusFilters: ['NEW', 'Flight Scheduled', 'Flight Completed', 'Flight Canceled', 'Dead Lead'],
+
+    leads: [],
 })
 
 
--- client/main.js
+++ client/main.js
@@ -1,8 +1,17 @@
 import data from './data.js'
 
+
 const app = new Vue({
     el: '#app',
     data,
+    computed: {
+        changeFormMenu() {
+			return this.id === null ? 'New Lead' : 'Update Lead'
+        },
+        changeFormSubmit() {
+            return this.id === null ? 'Create Lead' : 'Save Lead'
+        }
+    },
     methods: {
         addLead() {
             const lead = {
@@ -14,12 +23,85 @@ const app = new Vue({
                 notes: this.notes,
                 time: this.time,
                 type: this.type,
-                email: this.email
-
+                email: this.email,
+                id: Math.random(),
+                visible: true
             }
+            console.log(lead)
             this.leads.unshift(lead)
+            this.clear()
+        },
+
+        formatDate(date) {
+            return moment(date).format('ddd, MMMM-Do-YYYY')
+        },
+
+        setEditingId(id) {
+            this.id = id
+            this.dialog = true
+            const indexOfLead = this.leads.findIndex(lead => lead.id === id)
+            this.name = this.leads[indexOfLead].name
+            this.date = this.leads[indexOfLead].date
+            this.time = this.leads[indexOfLead].time
+            this.phone = this.leads[indexOfLead].phone
+            this.address = this.leads[indexOfLead].address
+            this.email = this.leads[indexOfLead].email
+            this.type = this.leads[indexOfLead].type
+            this.notes = this.leads[indexOfLead].notes
+            
+        },
+
+        updateLead(id) {
+            const indexOfLead = this.leads.findIndex(lead => lead.id === id)
+			const updatedLead = {
+				id: this.id,
+				name: this.name,
+                date: this.date,
+                time:this.time,
+                phone: this.phone,
+                address: this.address,
+                email: this.email,
+                type: this.type,
+                notes: this.notes,
+                show: true
+                
+            } 
+            this.leads[indexOfLead] = updatedLead
+        },
+        
+        saveLead() {
+            if (this.id !== null) {
+                this.updateLead(this.id)
+                this.close()  
+                this.show = false  
+            } else {
+                this.addLead()
+                this.close()
+                this.show = false
+            }
+        },
+
+        deleteLead(lead) {
+            this.leads.splice(this.leads.indexOf(lead), 1)
+        },
+
+        close() {
+            this.clear()
             this.dialog = false
-        } 
+            
+        },
+
+        clear() {
+            this.name = ''
+            this.address = ''
+            this.phone = ''
+            this.date = null
+            this.notes = ''
+            this.time = null
+            this.type = null
+            this.email = ''
+            this.id = null
+        }
     }
 
 })
