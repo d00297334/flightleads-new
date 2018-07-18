8f4be0934a8c97bfa5540ddd7e372f15b231d22f
Neil Gardner <ngardner@CIT-SCC112-1111.cs.dixie.edu>
2018-07-18 12:38:54 -0600

    simple server

--- client/data.js
+++ client/data.js
@@ -77,47 +77,6 @@ const data = () => ({
     },
 
 
-    leads: [
-        {
-            name: 'event 1',
-            type: 'Construction',
-            date:  'July 30th, 2018',
-            address:  '123 St. Utah',
-            time: '12:30',
-            notes: 'these are extra notes',
-            show: false,
-        },
-        {
-            name: 'event 2',
-            type: 'Wedding',
-            date:  'July 30th, 2018',
-            address:  '123 St. Utah',
-            time: '4:10',
-            notes:'notes',
-            show: false,
-        },
-        {
-            name: 'event 3',
-            type: 'Construction',
-            date:  'July 30th, 2018',
-            address:  '123 St. Utah',
-            time: '12:30',
-            notes: 'these are extra notes',
-            show: false,
-        },
-        {
-            name: 'event 4',
-            type: 'Wedding',
-            date:  'July 30th, 2018',
-            address:  '123 St. Utah',
-            time: '4:10',
-            notes:'some notes',
-            show: false,
-        }
-
-    ],
-
-
 
     dateItems: ['Today', 'This Week', 'This Month'],
     typeItems: ['Videography', 'Photography', 'Land/Site Survey', 'Real Estate', 'Outdoor/Extreme Sports - Event', 'Outdoor/Extreme Sports - Personal', 'Other'],
