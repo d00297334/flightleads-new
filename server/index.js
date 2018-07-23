a104de3d41adff30c2fad8e02ccf740940571c78
Kath <kathleen.cram@dmail.dixie.edu>
2018-07-23 09:36:12 -0600

    nodemail and server sockets

--- server/index.js
+++ server/index.js
@@ -1,9 +1,43 @@
 require('dotenv').config()
 
 const express = require('express')
+const nodeMailer = require('nodemailer')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const app = express()
+
+
+//email stuff
+app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({entended: true}))
+
+app.post('/send-email', function (req, res) {
+    let transporter = nodeMailer.createTransport({
+        host: 'box575.bluehost.com',
+        port: 465,
+        secure: true,
+        auth: {
+            user: 'notifications@flightleadscrm.com',
+            pass: 'FLemail123!'
+        }
+    })
+    let mailOptions = {
+        from: "'Flight Leads' <notifications@flightleadscrm.com>",
+        to: req.body.to,
+        subject: req.body.subject,
+        text: req.body.body,
+        heml: req.body.body
+    }
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error)
+        }
+        console.log('Message %s sent: %s', info.messageId, info.response)
+        res.render('index')
+    })
+
+})
+
 
 const leadsRouter = require('./routes/leads')
 
@@ -33,10 +67,13 @@ app.use((req, res, next) => {
   }
 })
 
+const port = 3000
 mongoose.connect(process.env.DB_URL)
   .then(() => {
 
     console.log("Databse Working")
-    app.listen(3000)
+    app.listen(port, function(req, res){
+        console.log('server is running at port ', port)
+    })
 
   })
